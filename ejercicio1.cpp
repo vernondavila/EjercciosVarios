90db1fca321d52a6dafee4e4980b1da36bf997f9
vernondavila <vernon.davila15877@est.uca.edu.ni>
2023-05-18 21:39:38 -0600

    ejercicios2

---
+++ ejercicio1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+void ContarNumeros(){
+    for(int i = 1; i <= 10; i++){
+        cout << i << endl;
+    }
+    
+}
+
+int main(int argc, char const *argv[]){
+    ContarNumeros();
+    return 0;
+}
